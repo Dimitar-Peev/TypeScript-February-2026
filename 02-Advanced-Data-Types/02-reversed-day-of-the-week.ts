9b22d3e4f0b6f0ab97ca4d8e1146aa52770c1c66
Dimitar-Peev <mctrix87@abv.bg>
2026-04-10 16:13:11 +0300

    Code optimization

--- 02-Advanced-Data-Types/02-reversed-day-of-the-week.ts
+++ 02-Advanced-Data-Types/02-reversed-day-of-the-week.ts
@@ -1,16 +1,18 @@
-function reverseDayOfTheWeek(day: string): void {
-    enum Days {
-        Monday = 1,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday
-    }
-    console.log(Days[day as keyof typeof Days] ?? 'error');
+enum Days {
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
 }
 
-reverseDayOfTheWeek('Monday'); // 1
-reverseDayOfTheWeek('Friday'); // 5
-reverseDayOfTheWeek('Invalid'); // error
+function printNumberOrError(day: string): void {
+    const index = Days[day as keyof typeof Days];
+    console.log(index ?? 'error');
+}
+
+printNumberOrError('Monday');  // 1
+printNumberOrError('Friday');  // 5
+printNumberOrError('Invalid'); // error
