c910f15da67b7b1a1cd3da47ec98dc4c8706669f
Dimitar-Peev <mctrix87@abv.bg>
2026-04-30 19:44:00 +0300

    Add APIService class for handling CRUD operations

---
+++ 08-Workshop/demo-project/src/services/api.ts
@@ -0,0 +1,50 @@
+export class APIService<T> {
+    private serviceUrl: string;
+
+    constructor(serviceUrl: string) {
+        this.serviceUrl = serviceUrl;
+    }
+
+    public async getAll(): Promise<T[]> {
+        const response = await fetch(`${this.serviceUrl}`);
+        const data = await response.json();
+        return data as T[];
+    }
+
+    public async getById(id: number): Promise<T> {
+        const response = await fetch(`${this.serviceUrl}/${id}`);
+        const data = await response.json();
+        return data as T;
+    }
+
+    public async create(itemData: T): Promise<T> {
+        const response = await fetch(this.serviceUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(itemData)
+        });
+        const data = await response.json();
+        return data as T;
+    }
+
+    public async update(id: number, itemData: T): Promise<T> {
+        const response = await fetch(`${this.serviceUrl}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(itemData)
+        });
+        const data = await response.json();
+        return data as T;
+    }
+
+    public async delete(id: number): Promise<void> {
+        await fetch(`${this.serviceUrl}/${id}`, {
+            method: 'DELETE',
+        });
+    }
+
+}
