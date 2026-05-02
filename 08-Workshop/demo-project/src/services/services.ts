6c6a8cdc66f510f981e1e1aa9b703904ae4f8894
Dimitar-Peev <mctrix87@abv.bg>
2026-05-02 12:02:17 +0300

    Add services module for instantiating PostsService and UsersService

---
+++ 08-Workshop/demo-project/src/services/services.ts
@@ -0,0 +1,5 @@
+import {PostsService} from "./posts.service.ts";
+import {UsersService} from "./users.service.ts";
+
+export const postsService = new PostsService();
+export const usersService = new UsersService();
