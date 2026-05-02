020afebf1f5cea72cf409f79fb7ac0d896ac4e14
Dimitar-Peev <mctrix87@abv.bg>
2026-05-02 12:53:01 +0300

    Refactor UsersService to extend APIService with CreateUser type

--- 08-Workshop/demo-project/src/services/users.service.ts
+++ 08-Workshop/demo-project/src/services/users.service.ts
@@ -1,8 +1,8 @@
 import {BASE_URL} from "../constants.ts";
-import type {User} from "../interfaces/user.interface.ts";
+import type {CreateUser, User} from "../interfaces/user.interface.ts";
 import {APIService} from "./api.ts";
 
-export class UsersService extends APIService<User> {
+export class UsersService extends APIService<User, CreateUser> {
     constructor() {
         super(`${BASE_URL}/users`);
     }
