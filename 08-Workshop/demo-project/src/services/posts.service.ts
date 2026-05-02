89cd526346c68625c593972f58017f84f9a8617e
Dimitar-Peev <mctrix87@abv.bg>
2026-05-02 12:52:31 +0300

    Refactor PostsService to extend APIService with CreatePost type

--- 08-Workshop/demo-project/src/services/posts.service.ts
+++ 08-Workshop/demo-project/src/services/posts.service.ts
@@ -1,8 +1,8 @@
 import {BASE_URL} from "../constants.ts";
-import type {Post} from "../interfaces/post.interface.ts";
+import type {CreatePost, Post} from "../interfaces/post.interface.ts";
 import {APIService} from "./api.ts";
 
-export class PostsService extends APIService<Post> {
+export class PostsService extends APIService<Post , CreatePost> {
     constructor() {
         super(`${BASE_URL}/posts`);
     }
